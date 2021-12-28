0a8e9f9943932998a37b3a0f16a37cacbc9d3ab7
Park Jeongsoo <wjdtn4383@daum.net>
2021-12-28 23:11:26 +0900

    MapStruct RequestUser -> UserDto Mapping 적용

--- user-service/src/main/java/dev/jsoo/userservice/controller/UserController.java
+++ user-service/src/main/java/dev/jsoo/userservice/controller/UserController.java
@@ -1,13 +1,20 @@
 package dev.jsoo.userservice.controller;
 
+import dev.jsoo.userservice.dto.UserDto;
+import dev.jsoo.userservice.mapStruct.UserMapper;
 import dev.jsoo.userservice.service.UserService;
 import dev.jsoo.userservice.vo.Greeting;
+import dev.jsoo.userservice.vo.RequestUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class UserController {
@@ -28,8 +35,16 @@ public class UserController {
         return "It's Working in User Service";
     }
 
-    @GetMapping("welcome")
+    @GetMapping("/welcome")
     public String welcome() {
         return greeting.getMessage();
+    }
+
+    @PostMapping("/user")
+    public String createUser(@RequestBody RequestUser user) {
+        UserDto userDto = UserMapper.INSTANCE.requestToDto(user);
+        log.info("userDto : {}", userDto.toString());
+        userService.createUser(userDto);
+        return "create user success";
     }
 }
