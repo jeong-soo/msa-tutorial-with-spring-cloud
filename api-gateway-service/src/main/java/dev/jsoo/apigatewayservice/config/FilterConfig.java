d31f86b1c312f8d3427c26d5aa28f4cd897deb90
박정수 <gustn4383@naver.com>
2021-12-31 01:01:56 +0900

    GATEWAY SERVICE 철자 틀림 수정

---
+++ api-gateway-service/src/main/java/dev/jsoo/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,11 @@
+package dev.jsoo.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    
+}
